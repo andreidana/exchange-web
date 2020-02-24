9704158cccb9315767531b4ce1c7fa33b53ad909
System Administrator <root@192-168-0-100.rdsnet.ro>
2020-02-24 21:13:51 +0200

    initial commit

---
+++ exchange-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'exch-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'exchange-web';
+}
